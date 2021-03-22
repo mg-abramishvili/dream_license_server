b034c7fd6293b96151fece691f23ef0306d4b636
Mikhail Abramishvili <mg@abramishvili.net>
2021-03-22 22:02:45 +0500

    .

--- resources/views/keys/index.blade.php
+++ resources/views/keys/index.blade.php
@@ -14,17 +14,17 @@
                         ПО
                     </th>
                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Статус
-                    </th>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                         Комментарий
                     </th>
                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                         Параметры
                     </th>
-                    <th scope="col" class="relative px-6 py-3">
-                        <span class="sr-only">Удалить</span>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Статус
                     </th>
+                    <!--<th scope="col" class="relative px-6 py-3">
+                        <span class="sr-only">Удалить</span>
+                    </th>-->
                 </tr>
             </thead>
             <tbody class="bg-white divide-y divide-gray-200">
@@ -41,13 +41,6 @@
                                 @foreach($key->programs as $program)
                                     {{ $program->title }}
                                 @endforeach
-                            </div>
-                        </div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                        <div class="flex items-center">
-                            <div class="text-sm font-medium text-gray-900">
-                                {{ $key->status }}
                             </div>
                         </div>
                     </td>
@@ -117,9 +110,20 @@
                             </div>
                         </div>
                     </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                        <a href="/keys/delete/{{ $key->id }}" class="text-indigo-600 hover:text-indigo-900">Удалить</a>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                        <div class="flex items-center">
+                            <div class="text-sm font-medium text-gray-900 @if($key->status == 'waiting') bg-yellow-300 @elseif($key->status == 'active') bg-green-300 @endif">
+                                @if($key->status == 'waiting')
+                                    ожидает активации
+                                @elseif($key->status == 'waiting')
+                                    активирован
+                                @endif
+                            </div>
+                        </div>
                     </td>
+                    <!--<td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                        <a href="/keys/delete/{{ $key->id }}" class="text-indigo-600 hover:text-indigo-900">Удалить</a>
+                    </td>-->
                 </tr>
                 @endforeach
             </tbody>
