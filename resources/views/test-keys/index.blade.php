92b5defe7d4a870d98bc1cd5c21f3fee9a4c172f
Mikhail Abramishvili <mg@abramishvili.net>
2021-04-07 12:45:04 +0500

    .

--- resources/views/test-keys/index.blade.php
+++ resources/views/test-keys/index.blade.php
@@ -1,9 +1,14 @@
 @extends('layouts.app')
 @section('content')
 
-    <a href="/keys/create" class="items-center px-8 py-2 mt-4 mb-4 mr-2 font-semibold text-white transition duration-500 ease-in-out transform bg-blue-700 border rounded-lg lg:inline-flex lg:mt-px hover:border-blue-700 hover:bg-blue-600 hover:text-white focus:ring focus:outline-none">Добавить ключ</a>
-
-    <h1 class="text-3xl mt-0 mb-6">Тестовые ключи</h1>
+    <div class="flex flex-wrap w-full">
+        <div class="inline-flex w-1/2">
+            <h1 class="text-3xl mt-0 mb-6">Тестовые ключи</h1>
+        </div>
+        <div class="inline-flex w-1/2 justify-end">
+            <a href="/keys/create" class="items-center px-8 py-2 mt-4 mb-4 mr-2 font-semibold text-white transition duration-500 ease-in-out transform bg-blue-700 border rounded-lg lg:inline-flex lg:mt-px hover:border-blue-700 hover:bg-blue-600 hover:text-white focus:ring focus:outline-none">Добавить ключ</a>
+        </div>
+    </div>
 
     <div class="w-full shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
         <table class="min-w-full divide-y divide-gray-200">
